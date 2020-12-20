12696ebfb455655ae2ed428c0d34aeb05d090d4a
Kevin Kekule <k.kekule@web.de>
2020-12-20 13:58:14 +0100

    removed unnecessary echos, forgot em in code^^

--- script-files/install-metallb.sh
+++ script-files/install-metallb.sh
@@ -38,13 +38,10 @@ external_ip="";
 while true; do
   sleep 1
   external_ip=$(kubectl get svc nginx --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}");
-  echo "uhm"
-  echo $external_ip
   if [[ -n "$external_ip" ]]; then
     count=$((count+1))
     break
   fi
-  echo "buhm"
   if [[ $count -gt 3 ]]; then
     echo "we waited long enough for the ip address, should already be finished.. stopping"
     return 1
